aebc284dd6b961f8244d0477e1e6c3a28bf8ba3e
white <chenqing0718@163.com>
2019-06-27 16:42:56 +0800

    Initial Commit

---
+++ RACHHTest/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  RACHHTest
+//
+//  Created by White-C on 2019/6/27.
+//  Copyright © 2019 White-C. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
