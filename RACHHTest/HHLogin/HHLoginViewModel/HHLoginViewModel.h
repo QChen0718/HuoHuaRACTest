2278bcd837cf6b09d3583bbcc845a9930fa0d75c
white <chenqing0718@163.com>
2019-06-28 19:31:49 +0800

    添加登录视图模型

---
+++ RACHHTest/HHLogin/HHLoginViewModel/HHLoginViewModel.h
@@ -0,0 +1,17 @@
+//
+//  HHLoginViewModel.h
+//  RACHHTest
+//
+//  Created by White-C on 2019/6/28.
+//  Copyright © 2019 White-C. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface HHLoginViewModel : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
