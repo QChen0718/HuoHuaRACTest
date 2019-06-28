4bed7983a6f52c8eead8f63513d17b0d38efd045
white <chenqing0718@163.com>
2019-06-28 21:48:44 +0800

    添加baseview

---
+++ RACHHTest/HHBaseView.h
@@ -0,0 +1,17 @@
+//
+//  HHBaseView.h
+//  RACHHTest
+//
+//  Created by White-C on 2019/6/28.
+//  Copyright © 2019 White-C. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface HHBaseView : UIView
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ RACHHTest/HHBaseView.m
@@ -0,0 +1,21 @@
+//
+//  HHBaseView.m
+//  RACHHTest
+//
+//  Created by White-C on 2019/6/28.
+//  Copyright © 2019 White-C. All rights reserved.
+//
+
+#import "HHBaseView.h"
+
+@implementation HHBaseView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
