f9f69681f85de5ebb8caa17fa9782fc2dd13a59f
white <chenqing0718@163.com>
2019-07-08 15:53:54 +0800

    添加帖子详情view

---
+++ RACHHTest/HHHome/HHView/HHAudioDetailView.h
@@ -0,0 +1,17 @@
+//
+//  HHAudioDetailView.h
+//  RACHHTest
+//
+//  Created by White-C on 2019/7/8.
+//  Copyright © 2019 White-C. All rights reserved.
+//
+
+#import "HHBaseView.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface HHAudioDetailView : HHBaseView
+
+@end
+
+NS_ASSUME_NONNULL_END
