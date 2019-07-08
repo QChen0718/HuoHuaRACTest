a4f28aa6d371dd5b2df3deab5bbab5375e1ecbf6
white <chenqing0718@163.com>
2019-07-08 16:07:29 +0800

    添加viewmodel

---
+++ RACHHTest/HHHome/HHViewModel/HHAudioDetailViewModel.h
@@ -0,0 +1,17 @@
+//
+//  HHAudioDetailViewModel.h
+//  RACHHTest
+//
+//  Created by White-C on 2019/7/8.
+//  Copyright © 2019 White-C. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface HHAudioDetailViewModel : HHBaseViewModel
+
+@end
+
+NS_ASSUME_NONNULL_END
