7c84d66e77357ac7579680e9cbe3229e1bee4b83
white <chenqing0718@163.com>
2019-07-08 15:20:11 +0800

    创建帖子详情页

---
+++ RACHHTest/HHHome/HHView/HHAudioDetailView.m
@@ -0,0 +1,21 @@
+//
+//  HHAudioDetailView.m
+//  RACHHTest
+//
+//  Created by White-C on 2019/7/8.
+//  Copyright © 2019 White-C. All rights reserved.
+//
+
+#import "HHAudioDetailView.h"
+
+@implementation HHAudioDetailView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
---
+++ RACHHTest/HHHome/HHViewController/HHAudioDetailViewController.h
@@ -0,0 +1,17 @@
+//
+//  HHAudioDetailViewController.h
+//  RACHHTest
+//
+//  Created by White-C on 2019/7/8.
+//  Copyright © 2019 White-C. All rights reserved.
+//
+
+#import "HHBaseViewController.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface HHAudioDetailViewController : HHBaseViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
