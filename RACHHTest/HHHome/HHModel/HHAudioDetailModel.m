54f28b430999fa7c82408efd4aaadb5a691b874f
white <chenqing0718@163.com>
2019-07-08 22:28:58 +0800

    添加详情模型

---
+++ RACHHTest/HHHome/HHModel/HHAudioDetailModel.m
@@ -0,0 +1,13 @@
+//
+//  HHAudioDetailModel.m
+//  RACHHTest
+//
+//  Created by White-C on 2019/7/8.
+//  Copyright © 2019 White-C. All rights reserved.
+//
+
+#import "HHAudioDetailModel.h"
+
+@implementation HHAudioDetailModel
+
+@end
